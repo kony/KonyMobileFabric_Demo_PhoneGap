8e211b196856eb796dcb05df976b5468980db2b3
Sreenivasulu <sreenivasulu.nallakshyam@kony.com>
2015-04-22 17:09:53 +0530

    Update password

--- kony/platforms/ios/www/js/config.js
+++ kony/platforms/ios/www/js/config.js
@@ -1,8 +1,8 @@
 // configure meta data here
 
 var MBaaSConfig ={
-                "SFUsername" : "sreenivasn@gmail.com",
-    			"SFPassword" : "Kony@(877uJ8SgCQtfWM8h64QgzSUJxu",   
+                //"SFUsername" : "sreenivasn@gmail.com",
+    			//"SFPassword" : "Kony@(877uJ8SgCQtfWM8h64QgzSUJxu",   
                 "AppKey" : "beca5f6ba7464fe3aca5d59f94ab1e27",
     			"AppSecret" : "45e2fb15385d44cc00f3303e95f3abd2",
     			"ServiceURL" : "https://100003520.auth.konycloud.com/appconfig",
