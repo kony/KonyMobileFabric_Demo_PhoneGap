eb689a8543fc83647d9f954d1f448279d2221e20
KonyDev <platform_integration_team@kony.com>
2015-05-26 13:41:06 +0530

    Update config.js

--- kony/www/js/config.js
+++ kony/www/js/config.js
@@ -1,8 +1,9 @@
 
+
 MBaaSConfig ={
     "DefaultUsername" : "sreenivasn@gmail.com", 
     "DefaultPassword" : "Kony@#214Q2HZyOWh5WcwIz2BlmF3N8LD",    
-    "AppKey" : "68fac5743b3667b1d20dbd97485f6208", 
+    "AppKey" : "68fac5743b3667b1d20dbd97485f6203", 
     "AppSecret" : "29971de24e4a912bf26c4184c4209585", 
     "IdentityService" : "SyncDemoIdService",
     "AccountIntegrationService":"AccountIntgService",
@@ -12,7 +13,7 @@ MBaaSConfig ={
     "ServiceURL" : "https://100003520.auth.konycloud.com/appconfig",
     "KMSID":"sreenivasulu.reddy@kony.com",    
     "getContactQuery":{ "queryString":"Select Id, FirstName, LastName, Title, AccountId, Phone, email, Department,Fax,ReportsToId, BirthDate, IsDeleted, LastModifiedDate from Contact"},
-    "getAccountQuery":{ "queryString":"Select Id, AccountNumber, Name, Type, Rating,IsDeleted,Phone,Fax,LastModifiedDate, Website, AnnualRevenue, BillingAddress from Account"},
+    "getAccountQuery":{ "queryString":"Select Id, AccountNumber, Name, Type, Rating,IsDeleted,Phone,Fax,LastModifiedDate, Website, AnnualRevenue, BillingAddress, BillingCity, BillingStreet, BillingState from Account"},
     "getLeadQuery":   { "queryString":"SELECT ID,NAME,FIRSTNAME,LASTNAME,TITLE,PHONE,EMAIL,IsDeleted, LastModifiedDate FROM Lead"},
-    "getOpportunityQuery":{ "queryString":"Select Id, Name, AccountId, Type, IsDeleted,LastModifiedDate, CloseDate, StageName  from Opportunity"}
+    "getOpportunityQuery":{ "queryString":"Select Id, Name, AccountId, Type, IsDeleted,LastModifiedDate, CloseDate, StageName, probability, Amount, LeadSource, ExpectedRevenue  from Opportunity"}
 };
